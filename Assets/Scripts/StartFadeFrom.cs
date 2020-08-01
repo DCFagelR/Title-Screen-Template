6660f7847ccf445f770e8c6870f074b230fb1bc2
Reinhart <fagelrdc@gmail.com>
2020-08-01 16:32:10 -0700

    removed find; now assigned by unity editor

--- Assets/Scripts/StartFadeFrom.cs
+++ Assets/Scripts/StartFadeFrom.cs
@@ -2,11 +2,15 @@
 
 public class StartFadeFrom : MonoBehaviour
 {
-    BlackFade fader => GameObject.Find("FadingCanvas").GetComponent<BlackFade>();
+    // BlackFade fader => GameObject.Find("FadingCanvas").GetComponent<BlackFade>();
+
+    [SerializeField]
+    private GameObject fader;
 
     // Start is called before the first frame update
     void Start()
     {
-        fader.FadeController();
+        // fader.FadeController();
+        fader.GetComponent<BlackFade>().FadeController();
     }
 }
