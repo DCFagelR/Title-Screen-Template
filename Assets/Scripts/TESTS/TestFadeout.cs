0947c3785fee84a5463f8d69f2e2243f4ff766ae
Reinhart <fagelrdc@gmail.com>
2020-07-31 19:34:18 -0700

    Added fadein test

---
+++ Assets/Scripts/TESTS/TestFadeout.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TestFadeout : MonoBehaviour
+{
+    BlackFade fader => GameObject.Find("FadingCanvas").GetComponent<BlackFade>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            fader.FadeController(false);
+        }
+    }
+}
