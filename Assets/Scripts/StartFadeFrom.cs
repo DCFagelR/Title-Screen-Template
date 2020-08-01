075ad3d9d4ed64f57e0ad7b85e76ee5720a948ad
Reinhart <fagelrdc@gmail.com>
2020-07-31 19:20:23 -0700

    Added fadein/fadeout options and easy to call

---
+++ Assets/Scripts/StartFadeFrom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class StartFadeFrom : MonoBehaviour
+{
+    BlackFade fader => GameObject.Find("FadingCanvas").GetComponent<BlackFade>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fader.FadeController();
+    }
+}
