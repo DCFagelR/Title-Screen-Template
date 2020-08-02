c649e3613f6bcedf314c7b3dbab8ea9b1cdeb884
Deshon Cacho <iamverysuperfat@gmail.com>
2020-08-02 05:03:57 -0700

    Added FadeTo button & prefabs

---
+++ Assets/Scripts/FadeToButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeToButton : MonoBehaviour
+{
+    BlackFade fader => GameObject.Find("FadingCanvas").GetComponent<BlackFade>();
+
+    public void OnButtonPress()
+    {
+        fader.FadeController(false);
+    }
+}
