09cda690e231e12b283f033ba19311e234a151e4
Reinhart <fagelrdc@gmail.com>
2020-08-04 16:44:21 -0700

    Menu resize; inheritance changes

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public static void QuitGame()
+    {
+        Debug.Log("The game has quit!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ExtendMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtendMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Background;
+
+    private GameObject myObject;
+
+    // Start is called before the first frame update
+    void Update() // Change to awake
+    {
+        myObject = this.gameObject.gameObject;
+
+        if(Input.GetKey(KeyCode.Space)) {
+            myObject.SetActive(false);
+        }
+    }
+}
