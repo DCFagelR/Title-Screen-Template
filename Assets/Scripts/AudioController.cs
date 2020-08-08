fb821fdcb88e3293697332005c58ee3f3d4f5b02
Deshon Cacho <iamverysuperfat@gmail.com>
2020-08-08 00:24:56 -0700

    Reworked background music implementation

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -4,7 +4,7 @@ public class AudioController : MonoBehaviour
 {
 // ++Variables+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    private AudioSource _audioSource1, _audioSource2, _audioSource3;
+    private AudioSource _audioSource1, _audioSource2;
 
     public AudioClip buttonSelect;
     [Range(0,1)] public float buttonSelectVolume = 1;
@@ -16,22 +16,12 @@ public class AudioController : MonoBehaviour
     public bool buttonPressPlayOnAwake = true;
     public bool buttonPressLoop = true;
 
-    public AudioClip backgroundMusic;
-    [Range(0,1)] public float backgroundMusicVolume = 1;
-    public bool backgroundMusicPlayOnAwake = true;
-    public bool backgroundMusicLoop = true;
-
 // ++Methods+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     private void Start()
     {
         _audioSource1 = AddAudio(buttonSelect, buttonSelectVolume, buttonSelectPlayOnAwake, buttonSelectLoop);
         _audioSource2 = AddAudio(buttonPress, buttonPressVolume, buttonPressPlayOnAwake, buttonPressLoop);
-        _audioSource3 = AddAudio(backgroundMusic, backgroundMusicVolume, backgroundMusicPlayOnAwake, backgroundMusicLoop);
-    
-        PlaySounds(_audioSource1);
-        PlaySounds(_audioSource2);
-        PlaySounds(_audioSource3);
     }
     
 
@@ -46,14 +36,6 @@ public class AudioController : MonoBehaviour
         newAudio.loop = loop;
 
         return newAudio;
-    }
-
-// --------------------------------------------------------------------------------------------------
-
-    private void PlaySounds(AudioSource audioSource)
-    {
-        if(audioSource.playOnAwake)
-            audioSource.Play();
     }
 
 // --------------------------------------------------------------------------------------------------
