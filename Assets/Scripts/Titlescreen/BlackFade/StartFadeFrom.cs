b399c6974838ce9616d3a9a1298e8a481b2096e7
Reinhart <fagelrdc@gmail.com>
2020-08-07 23:09:46 -0700

    restored needed scripts

---
+++ Assets/Scripts/Titlescreen/BlackFade/StartFadeFrom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class StartFadeFrom : MonoBehaviour
+{
+    [SerializeField]
+    private BlackFade _fader = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _fader.FadeController();
+    }
+}
---
+++ Assets/Scripts/Titlescreen/BlackFade/StartFadeTo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class StartFadeTo : MonoBehaviour
+{
+    [SerializeField]
+    private BlackFade _fader = null;
+
+    public void Awake()
+    {
+        _fader.FadeController(false);
+    }
+}
