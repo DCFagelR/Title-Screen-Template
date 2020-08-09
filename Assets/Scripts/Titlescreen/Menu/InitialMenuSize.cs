9b47f5c0f77cec131892d8da98d13dee829cb2ef
Reinhart <fagelrdc@gmail.com>
2020-08-08 20:04:38 -0700

    made Main Menu resizable with whatever the side menu size is

--- Assets/Scripts/Titlescreen/Menu/InitialMenuSize.cs
+++ Assets/Scripts/Titlescreen/Menu/InitialMenuSize.cs
@@ -5,15 +5,17 @@ public class InitialMenuSize : MonoBehaviour
 {
 
     private float ReferenceResolution => transform.parent.GetComponentInParent<CanvasScaler>().referenceResolution.x;
-    private float SideMenuRatio => transform.parent.GetComponent<RectTransform>().anchorMin.x;
+    private float SideMenuAnchorX => transform.parent.GetComponent<RectTransform>().anchorMin.x;
 
     void Start()
     {
         Vector2 mainInitialSize = GetComponent<RectTransform>().offsetMin;
+        float SideMenuRatio = 1 - SideMenuAnchorX;
 
         // Need reference resolution as canvas scales
-        mainInitialSize.x = ReferenceResolution * SideMenuRatio;
+        mainInitialSize.x = ReferenceResolution * SideMenuAnchorX;
 
         GetComponent<RectTransform>().offsetMin = mainInitialSize;
+        GetComponent<RectTransform>().anchorMin = new Vector2(-SideMenuAnchorX/SideMenuRatio, 0);
     }
 }
