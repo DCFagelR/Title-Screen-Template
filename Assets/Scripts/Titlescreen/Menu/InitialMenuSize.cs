f9dd87ad56213993907457e50ec8a506905b9d37
Reinhart <fagelrdc@gmail.com>
2020-08-09 21:25:34 -0700

    MenuRoot now initializes the submenu sizes

---
+++ Assets/Scripts/Titlescreen/Menu/InitialMenuSize.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialMenuSize : MonoBehaviour
+{
+
+// ++Variables+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    private float ReferenceResolution => transform.parent.GetComponentInParent<CanvasScaler>().referenceResolution.x;
+
+    [SerializeField]
+    private GameObject _sideMenu;
+    [SerializeField]
+    private GameObject _mainMenu;
+
+    [SerializeField]
+    [Range(0,0.8f)]
+    private float _sideMenuMinAnchorX = 0.8f;
+    
+    private Vector2 _closedSize;
+
+// ++Methods+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    void Start()
+    {
+        setSideMenuSize();
+        setMainMenuSize();
+    }
+
+// ----------------------------------------------------------------------------
+
+    public Vector2 getClosedSize() => _closedSize;
+
+// ----------------------------------------------------------------------------
+
+    private void setSideMenuSize()
+    {
+        _sideMenu.GetComponent<RectTransform>().anchorMin = new Vector2(_sideMenuMinAnchorX, 0);
+        _sideMenu.GetComponent<RectTransform>().anchorMax = new Vector2(1, 1);
+
+        _sideMenu.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+        _sideMenu.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+    }
+
+// ----------------------------------------------------------------------------
+
+    private void setMainMenuSize()
+    {
+        float sideMinX = _sideMenu.GetComponent<RectTransform>().anchorMin.x;
+        float horizontalSize = ReferenceResolution * sideMinX;
+
+        _mainMenu.GetComponent<RectTransform>().anchorMin = Vector2.zero;
+        _mainMenu.GetComponent<RectTransform>().anchorMax = new Vector2(sideMinX, 1);
+
+        _mainMenu.GetComponent<RectTransform>().offsetMin = new Vector2(horizontalSize, 0);
+
+        _closedSize = _mainMenu.GetComponent<RectTransform>().sizeDelta;
+        
+        Debug.Log(_closedSize);
+    }
+}
